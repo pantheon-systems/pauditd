8bf5801d4b2fa538f18ee305ab9dbf06518177cd
Daniel Feinberg <daniel.feinberg@pantheon.io>
2019-04-23 10:49:59 -0600

    allow NODENAME env var to override hostname for pauditd reporting

---
+++ pkg/system/host_data_provider.go
@@ -0,0 +1,25 @@
+package system
+
+import (
+	"os"
+)
+
+const (
+	hostnameEnv string = "NODENAME"
+)
+
+// GetHostname retrieves the hostname of the system, if no hostname
+// is available we return empty string
+func GetHostname() string {
+	var err error
+
+	host, ok := os.LookupEnv(hostnameEnv)
+	if !ok {
+		host, err = os.Hostname()
+		if err != nil {
+			return ""
+		}
+	}
+
+	return host
+}
